1d9c1c732aace65d3e403529e46f0acab093ea93
Elene Lelashvili <elenelelashvili6@gmail.com>
2022-12-12 15:29:29 +0400

    feat: add control class

--- controls.js
+++ controls.js
@@ -8,14 +8,40 @@ class Controls{
         this.#addKeyboardListeners();
     }
 
-    #addKeyboardListeners(ctx){
-        ctx.beginPath();
-        ctx.rect(
-            this.x - this.width/2,
-            this.y - this.height/2,
-            this.width,
-            this.height
-        );
-        ctx.fill();
+    #addKeyboardListeners(){
+        document.onkeydown = (event) => {
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
+            }
+            console.table(this);
+        }
+        document.onkeyup = (event) => {
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
+            }
+            console.table(this);
+        }
     }
 }
